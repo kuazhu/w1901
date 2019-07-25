f43cda86caa5ac6b589aeadb1638d4b52fadc65e
TomChen <76876201@qq.com>
2019-07-25 16:36:52 +0800

    todolist static page

--- 03-senior/nodejs/07-web/02-handle-static/static/js/index.js
+++ 03-senior/nodejs/07-web/02-handle-static/static/js/index.js
@@ -2,6 +2,13 @@
 * @Author: TomChen
 * @Date:   2019-07-25 15:17:42
 * @Last Modified by:   TomChen
-* @Last Modified time: 2019-07-25 15:17:45
+* @Last Modified time: 2019-07-25 16:33:43
 */
-alert('abc')
+;(function($){
+    var $input = $('.todo-input')
+    $input.on('keydown',function(ev){
+        if(ev.keyCode == 13){
+            //发送ajax
+        }
+    })
+})(jQuery)
