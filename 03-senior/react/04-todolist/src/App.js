670e581c97efbf3f2199e7be8248ee3638de02b4
TomChen <76876201@qq.com>
2019-08-09 15:20:28 +0800

    component;

---
+++ 03-senior/react/04-todolist/src/App.js
@@ -0,0 +1,15 @@
+/*
+* @Author: TomChen
+* @Date:   2019-08-09 15:14:36
+* @Last Modified by:   TomChen
+* @Last Modified time: 2019-08-09 15:17:00
+*/
+import React,{ Component } from 'react'
+
+class App extends Component{
+    render(){
+        return <div> here is a App </div>
+    }
+}
+
+export default App
---
+++ 03-senior/react/04-todolist/src/index.js
@@ -0,0 +1,12 @@
+/*
+* @Author: TomChen
+* @Date:   2019-08-09 10:53:03
+* @Last Modified by:   TomChen
+* @Last Modified time: 2019-08-09 15:15:02
+*/
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import App from './App.js'
+
+ReactDOM.render(<App />,document.getElementById('root'))
