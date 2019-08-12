66289896b234108be563f4567b60909cbb2174ca
TomChen <76876201@qq.com>
2019-08-12 11:00:38 +0800

    redux dispatch action

--- 03-senior/react/06-todolist-redux/src/App.js
+++ 03-senior/react/06-todolist-redux/src/App.js
@@ -1,19 +1,19 @@
 /*
-* @Author: TomChen
-* @Date:   2019-08-09 15:14:36
-* @Last Modified by:   TomChen
-* @Last Modified time: 2019-08-12 10:33:20
-*/
-import React,{ Component } from 'react'
+ * @Author: TomChen
+ * @Date:   2019-08-09 15:14:36
+ * @Last Modified by:   TomChen
+ * @Last Modified time: 2019-08-12 10:56:06
+ */
+import React, { Component } from 'react'
 
 import store from './store'
 
-import { Button,Input,Row,Col,List } from 'antd';
+import { Button, Input, Row, Col, List } from 'antd';
 
 import "./App.css"
 
-class App extends Component{
-    constructor(props){
+class App extends Component {
+    constructor(props) {
         super(props)
         /*
         this.state = {
@@ -22,37 +22,47 @@ class App extends Component{
         }
         */
         this.handleChange = this.handleChange.bind(this)
-        this.handleAdd=this.handleAdd.bind(this)
+        this.handleAdd = this.handleAdd.bind(this)
 
         this.state = store.getState()
+        store.subscribe(()=>{this.setState(store.getState())})
     }
-    handleAdd(){
-        this.setState((preState)=>({
-            list:[...preState.list,preState.task],
-            task:''
+    handleAdd() {
+        this.setState((preState) => ({
+            list: [...preState.list, preState.task],
+            task: ''
         }))
     }
-    handleChange(ev){
-      const task = ev.target.value
-      this.setState(()=>({
-         task:task
-      }))
-    }
-    handleDel(index){
-        const list = [...this.state.list]
-        list.splice(index,1)
+    handleChange(ev) {
+        const task = ev.target.value
+        /*
         this.setState(()=>({
+           task:task
+        }))
+        */
+        //派发action
+        //action就是一个对象
+        const action = {
+            type:"change_item",
+            payload:task
+        }
+        store.dispatch(action)
+    }
+    handleDel(index) {
+        const list = [...this.state.list]
+        list.splice(index, 1)
+        this.setState(() => ({
             list
         }))
     }
-    getItems(){
-        return this.state.list.map((item,index)=>{
-          return <Item key={index} task={item} handleDel={this.handleDel.bind(this,index)} />
-        })        
+    getItems() {
+        return this.state.list.map((item, index) => {
+            return <Item key={index} task={item} handleDel={this.handleDel.bind(this,index)} />
+        })
     }
-    render(){
-        return( 
-        <div className="App">
+    render() {
+        return (
+            <div className="App">
             <Row>
                 <Col span={18}>
                     <Input 
@@ -82,8 +92,8 @@ class App extends Component{
               )}
             />
             
-        </div> 
-        )             
+        </div>
+        )
     }
 }
 
--- 03-senior/react/06-todolist-redux/src/store/reducer.js
+++ 03-senior/react/06-todolist-redux/src/store/reducer.js
@@ -2,7 +2,7 @@
 * @Author: TomChen
 * @Date:   2019-08-12 10:29:05
 * @Last Modified by:   TomChen
-* @Last Modified time: 2019-08-12 10:34:51
+* @Last Modified time: 2019-08-12 10:58:11
 */
 const defaultState = {
     list:["吃饭","睡觉","敲代码"],
@@ -10,6 +10,11 @@ const defaultState = {
 }
 
 export default (state=defaultState,action)=>{
+    
+    if(action.type == 'change_item'){
+        //错误的写法
+        state.task = action.payload
+    }
 
     return state
 }
