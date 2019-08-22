c1e9bc1ea387ec1d55ff67ccd9fe5e42c6a9b03c
TomChen <76876201@qq.com>
2019-08-22 11:53:55 +0800

    bind event for user login

--- 04-project/kmall/home/src/pages/user-login/index.js
+++ 04-project/kmall/home/src/pages/user-login/index.js
@@ -2,9 +2,36 @@
 * @Author: TomChen
 * @Date:   2019-08-21 17:42:33
 * @Last Modified by:   TomChen
-* @Last Modified time: 2019-08-22 11:27:47
+* @Last Modified time: 2019-08-22 11:52:15
 */
 require('pages/common/logo')
 require('pages/common/footer')
 
 require('./index.css')
+
+
+var page = {
+    init:function(){
+        this.bindEvent()
+    },
+    bindEvent:function(){
+        var _this = this
+        $('#btn-submit').on('click',function(){
+            _this.submit()
+        })
+        $('input').on('keyup',function(ev){
+            if(ev.keyCode == 13){
+                _this.submit()
+            }
+        })
+    },
+    submit:function(){
+        //1.获取数据
+        //2.校验数据
+        //3.发送请求
+    }
+}
+
+$(function(){
+    page.init()
+})
