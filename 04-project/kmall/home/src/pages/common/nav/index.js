eddb63beb5589f2f1388e35a36f1fb44c9652c5c
TomChen <76876201@qq.com>
2019-08-22 11:14:28 +0800

    nav ui

---
+++ 04-project/kmall/home/src/pages/common/nav/index.js
@@ -0,0 +1,7 @@
+/*
+* @Author: TomChen
+* @Date:   2019-08-22 11:07:01
+* @Last Modified by:   TomChen
+* @Last Modified time: 2019-08-22 11:07:11
+*/
+require('./index.css')
--- 04-project/kmall/home/src/pages/index/index.js
+++ 04-project/kmall/home/src/pages/index/index.js
@@ -2,8 +2,9 @@
 * @Author: TomChen
 * @Date:   2019-08-21 17:42:33
 * @Last Modified by:   TomChen
-* @Last Modified time: 2019-08-22 10:26:11
+* @Last Modified time: 2019-08-22 11:07:26
 */
+require('pages/common/nav')
 require('pages/common/footer')
 
 require('./index.css')
