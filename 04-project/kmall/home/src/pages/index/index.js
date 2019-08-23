48738d932de2b3fb6f6cf1cd28b0e3723c1b9996
TomChen <76876201@qq.com>
2019-08-23 16:50:58 +0800

    add swiper

--- 04-project/kmall/home/src/pages/index/index.js
+++ 04-project/kmall/home/src/pages/index/index.js
@@ -2,12 +2,13 @@
 * @Author: TomChen
 * @Date:   2019-08-21 17:42:33
 * @Last Modified by:   TomChen
-* @Last Modified time: 2019-08-23 16:16:12
+* @Last Modified time: 2019-08-23 16:42:58
 */
+import Swiper from 'swiper'
 require('pages/common/nav')
 require('pages/common/search')
 require('pages/common/footer')
-
+require('swiper/dist/css/swiper.min.css')
 var api = require('api')
 var _util = require('util')
 
@@ -18,6 +19,7 @@ var categoriesTpl = require('./categoriesTpl.tpl')
 var page = {
     init:function(){
         this.loadHomeCategories()
+        this.loadSwiper()
     },
     loadHomeCategories:function(){
         api.getHomeCategories({
@@ -28,6 +30,22 @@ var page = {
                 $('.categories').html(html)
             }
         })
+    },
+    loadSwiper(){
+        var mySwiper = new Swiper ('.swiper-container', {
+            loop: true, // 循环模式选项
+            autoplay:true,
+            // 如果需要分页器
+            pagination: {
+              el: '.swiper-pagination',
+              clickable:true
+            },
+            // 如果需要前进后退按钮
+            navigation: {
+              nextEl: '.swiper-button-next',
+              prevEl: '.swiper-button-prev',
+            },
+        }) 
     }
 }
 
