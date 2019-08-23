afe8383f960dbc978932288391ac6e60b32b920c
TomChen <76876201@qq.com>
2019-08-23 10:38:54 +0800

    search input

---
+++ 04-project/kmall/home/src/pages/common/search/index.js
@@ -0,0 +1,34 @@
+/*
+ * @Author: TomChen
+ * @Date:   2019-08-21 17:42:33
+ * @Last Modified by:   TomChen
+ * @Last Modified time: 2019-08-23 10:36:42
+ */
+
+require('./index.css')
+
+var page = {
+    init: function() {
+        this.bindEvent()
+    },
+    bindEvent: function() {
+        var _this = this
+        $('#btn-search').on('click', function() {
+            _this.submit()
+        })
+        $('input').on('keyup', function(ev) {
+            if (ev.keyCode == 13) {
+                _this.submit()
+            }
+        })
+    },
+    submit: function() {
+        //1.获取数据
+        var keyword = $.trim($('#search-input').val())
+        window.location.href = './list.html?keyword='+keyword
+    }
+}
+
+$(function() {
+    page.init()
+})
--- 04-project/kmall/home/src/pages/index/index.js
+++ 04-project/kmall/home/src/pages/index/index.js
@@ -2,10 +2,10 @@
 * @Author: TomChen
 * @Date:   2019-08-21 17:42:33
 * @Last Modified by:   TomChen
-* @Last Modified time: 2019-08-22 17:05:01
+* @Last Modified time: 2019-08-23 10:34:48
 */
 require('pages/common/nav')
-
+require('pages/common/search')
 require('pages/common/footer')
 
 require('./index.css')
