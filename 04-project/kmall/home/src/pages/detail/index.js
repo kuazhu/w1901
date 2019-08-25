265b3b8d92f33db2fd2b8d5c524562e526d42a34
TomChen <76876201@qq.com>
2019-08-25 17:57:44 +0800

    switch img for detail page

--- 04-project/kmall/home/src/pages/detail/index.js
+++ 04-project/kmall/home/src/pages/detail/index.js
@@ -2,7 +2,7 @@
 * @Author: TomChen
 * @Date:   2019-08-21 17:42:33
 * @Last Modified by:   TomChen
-* @Last Modified time: 2019-08-25 17:38:39
+* @Last Modified time: 2019-08-25 17:55:31
 */
 require('pages/common/nav')
 require('pages/common/search')
@@ -19,12 +19,23 @@ var page = {
         id:_util.getParamFromUrl('productId')
     },    
     init:function(){
+        this.$elem = $('.detail-box')
         this.bindEvent()
         this.productsDetail()
     },
 
     bindEvent:function(){
         var _this = this
+        //1.图片切换
+        this.$elem.on('mouseenter','.product-small-img-item',function(){
+            var $this = $(this)
+            //小图片样式切换
+            $this.addClass('active')
+            .siblings('.product-small-img-item').removeClass('active')
+            //大图切换地址
+            var imgSrc = $this.find('img').attr('src')
+            $('.product-main-img img').attr('src',imgSrc)
+        })
  
     },
     productsDetail:function(){
@@ -38,11 +49,10 @@ var page = {
                 if(product){
                     product.images = product.images.split(',')
                     product.activeImage = product.images[0]
-                    console.log(product)
                     var html = _util.render(tpl,product)
-                    $('.detail-box').html(html)
+                    _this.$elem.html(html)
                 }else{
-                    $('.detail-box').html('<p class="empty-message">您找的商品去火星了!</p>')
+                    _this.$elem.html('<p class="empty-message">您找的商品去火星了!</p>')
                 }
             }
         })
