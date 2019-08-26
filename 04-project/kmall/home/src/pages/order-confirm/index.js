75cee60ab6d009607f28aca2770db175850f387f
TomChen <76876201@qq.com>
2019-08-26 15:54:00 +0800

    order confirm ui

---
+++ 04-project/kmall/home/src/pages/order-confirm/index.js
@@ -0,0 +1,27 @@
+/*
+* @Author: TomChen
+* @Date:   2019-08-21 17:42:33
+* @Last Modified by:   TomChen
+* @Last Modified time: 2019-08-26 15:49:13
+*/
+require('pages/common/nav')
+require('pages/common/search')
+require('pages/common/footer')
+
+var api = require('api')
+var _util = require('util')
+
+var tpl = require('./index.tpl')
+require('./index.css')
+
+var page = {
+  
+    init:function(){
+
+    },
+}
+
+
+$(function() {
+    page.init()
+})
--- 04-project/kmall/home/webpack.config.js
+++ 04-project/kmall/home/webpack.config.js
@@ -2,7 +2,7 @@
 * @Author: TomChen
 * @Date:   2019-08-08 16:30:19
 * @Last Modified by:   TomChen
-* @Last Modified time: 2019-08-26 10:22:19
+* @Last Modified time: 2019-08-26 15:50:17
 */
 
 const path = require('path')
@@ -29,6 +29,7 @@ module.exports = {
         'list'                  :'./src/pages/list/index.js',
         'detail'                :'./src/pages/detail/index.js',
         'cart'                  :'./src/pages/cart/index.js',
+        'order-confirm'         :'./src/pages/order-confirm/index.js',
         'user-login'            :'./src/pages/user-login/index.js',
         'user-register'         :'./src/pages/user-register/index.js',
         'result'                :'./src/pages/result/index.js',
@@ -105,6 +106,7 @@ module.exports = {
         new htmlWebpackPlugin(getHtmlConfig('list','列表页')),        
         new htmlWebpackPlugin(getHtmlConfig('detail','商品详情页')),        
         new htmlWebpackPlugin(getHtmlConfig('cart','购物车')),        
+        new htmlWebpackPlugin(getHtmlConfig('order-confirm','订单确认')),        
         new htmlWebpackPlugin(getHtmlConfig('user-login','用户登录')),        
         new htmlWebpackPlugin(getHtmlConfig('user-register','用户注册')),        
         new htmlWebpackPlugin(getHtmlConfig('result','结果提示页')),        
