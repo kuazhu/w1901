cc7cc2fa8fae54a9873fe9c1409b5e188d3f8e28
TomChen <76876201@qq.com>
2019-09-04 19:33:42 +0800

    config swiper

--- 04-project/kmall/mobile/src/components/tab-bar/index.vue
+++ 04-project/kmall/mobile/src/components/tab-bar/index.vue
@@ -1,10 +1,28 @@
 <template>
     <div class="TabBar">
-        <van-tabbar v-model="active">
-          <van-tabbar-item icon="home-o">标签</van-tabbar-item>
-          <van-tabbar-item icon="search">标签</van-tabbar-item>
-          <van-tabbar-item icon="friends-o">标签</van-tabbar-item>
-          <van-tabbar-item icon="setting-o">标签</van-tabbar-item>
+        <van-tabbar 
+            v-model="active"
+            active-color="#07c160"
+            inactive-color="#000"
+        >
+            <van-tabbar-item     
+            replace
+            to="/home"
+            icon="home-o">
+                首页
+            </van-tabbar-item>
+            <van-tabbar-item     
+            replace
+            to="/cart"
+            icon="cart-o">
+                购物车
+            </van-tabbar-item>
+            <van-tabbar-item     
+            replace
+            to="/me"
+            icon="friends-o">
+                个人中心
+            </van-tabbar-item>                         
         </van-tabbar>
     </div>
 </template>
--- 04-project/kmall/mobile/src/pages/home/index.vue
+++ 04-project/kmall/mobile/src/pages/home/index.vue
@@ -1,12 +1,34 @@
 <template>
     <div class="Home">
-        home page
+        <div class="swiper-container">
+            <div class="swiper-wrapper">
+                <div class="swiper-slide">Slide 1</div>
+                <div class="swiper-slide">Slide 2</div>
+                <div class="swiper-slide">Slide 3</div>
+            </div>
+            <!-- 如果需要分页器 -->
+            <div class="swiper-pagination"></div>
+        </div>
     </div>
 </template>
 
 <script>
+    import Swiper from 'swiper'
+    import 'swiper/dist/css/swiper.min.css'   
     export default {
-        name:'Home',       
+        name:'Home', 
+        mounted(){
+            new Swiper ('.swiper-container', {
+                // 循环模式选项
+                loop: true,
+                autoplay:true,
+                //分页器
+                pagination: {
+                  el: '.swiper-pagination',
+                  clickable:true
+                },
+            })
+        }              
     }
 </script>
 
