683e8887d0332914c3ae2d91b46605369067a50b
TomChen <76876201@qq.com>
2019-09-05 20:54:22 +0800

    miniprogram wx for

--- 04-project/miniprogram/pages/article/article.js
+++ 04-project/miniprogram/pages/article/article.js
@@ -5,7 +5,29 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    /*
+    text:'abc',
+    */
+    articles:[
+      {
+        avatar:'/images/avatar/u1.jpg',
+        date:'2019-10-10',
+        title:'我是文章标题1',
+        img:'/images/article/a1.jpg',
+        desc:'我是描述1',
+        star:30,
+        view:20
+      },
+      {
+        avatar: '/images/avatar/u2.jpg',
+        date: '2019-10-10',
+        title: '我是文章标题2',
+        img: '/images/article/a2.jpg',
+        desc: '我是描述2',
+        star: 30,
+        view: 20
+      },      
+    ]
   },
 
   /**
