599c9132b00386600591bd9bb63f45dde652dc57
TomChen <76876201@qq.com>
2019-09-06 18:26:18 +0800

    miniprogram lifecycle

--- 04-project/miniprogram/pages/article/article.js
+++ 04-project/miniprogram/pages/article/article.js
@@ -34,55 +34,55 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    //console.log('articel onLoad')
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+    //console.log('articel onReady')
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    //console.log('articel onShow')
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-
+    //console.log('articel onHide')
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-
+    //console.log('articel onUnload')
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-
+    //console.log('articel onPullDownRefresh')
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-
+    //console.log('articel onReachBottom')
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-
+    //console.log('articel onShareAppMessage')
   }
 })
--- 04-project/miniprogram/pages/index/index.js
+++ 04-project/miniprogram/pages/index/index.js
@@ -3,6 +3,61 @@
 const app = getApp()
 
 Page({
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    //console.log('index onLoad')
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    //console.log('index onReady')
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    //console.log('index onShow')
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    //console.log('index onHide')
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    //console.log('index onUnload')
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    //console.log('index onPullDownRefresh')
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    //console.log('index onReachBottom')
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    //console.log('index onShareAppMessage')
+  },  
   /*
   tapText:function(){
     console.log('tapText...')
@@ -17,8 +72,11 @@ Page({
       url:'/pages/article/article'
     })
     */
+    
+    
     wx.redirectTo({
       url: '/pages/article/article'
     })
+    
   }
 })
