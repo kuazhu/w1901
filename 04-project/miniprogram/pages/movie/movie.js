c29c6513bfe559e4c8878275ab260ccb49c9127b
TomChen <76876201@qq.com>
2019-09-08 15:18:52 +0800

    handle data

--- 04-project/miniprogram/pages/movie/movie.js
+++ 04-project/miniprogram/pages/movie/movie.js
@@ -12,7 +12,23 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    var _this = this
+    wx.request({
+      url:"http://t.yushu.im/v2/movie/in_theaters?start=0&count=3",
+      success:function(res){
+        var data = res.data.subjects.map(function(item){
+          return {
+            coverImg:item.images.large,
+            title:item.title,
+            score:item.rating.average,
+            stars:item.rating.stars
+          }
+        })
+        console.log(data)
+        _this.setData({movies:data})
 
+      }
+    })
   },
 
   /**
